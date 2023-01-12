169b1cb8ba39cb7f97469f061dc97f887b21658f
mesifer <brosfandy@gmail.com>
2023-01-12 12:49:13 +0700

    fix menubar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,25 +33,31 @@ export default function Navbar() {
                     <div
                         className={
                             isActive
-                                ? 'Navigasi text-[#ffffff8C] font-normal lg:static min-[320px]:flex-col min-[320px]:w-[100%] lg:w-[40%] min-[320px]:left-0 min-[320px]:px-4 lg:flex  min-[320px]:absolute min-[320px]:top-[100%] gap-x-8 md:pl-[60%] lg:pl-0'
-                                : 'Navigasi text-[#ffffff8C] font-normal lg:static min-[320px]:flex-col min-[320px]:w-[100%] lg:w-[40%] min-[320px]:left-0 min-[320px]:px-4 lg:flex hidden min-[320px]:absolute min-[320px]:top-[100%] gap-x-8 md:pl-[60%] lg:pl-0'
+                                ? 'Navigasi text-[#ffffff8C] font-normal lg:static min-[320px]:flex-col min-[320px]:w-[100%] lg:w-[40%] min-[320px]:left-0 min-[320px]:px-4 lg:flex  min-[320px]:absolute min-[320px]:top-[120%] gap-x-8 md:pl-[60%] lg:pl-0'
+                                : 'Navigasi text-[#ffffff8C] font-normal lg:static min-[320px]:flex-col min-[320px]:w-[100%] lg:w-[40%] min-[320px]:left-0 min-[320px]:px-4 lg:flex hidden min-[320px]:absolute min-[320px]:top-[120%] gap-x-8 md:pl-[60%] lg:pl-0'
                         }
                     >
-                        <div className="min-[320px]:bg-[#0A1220] min-[320px]:flex min-[320px]:flex-col lg:flex-row lg:bg-transparent min-[320px]:py-6 min-[320px]:px-8 min-[320px]:gap-y-6 min-[320px]:rounded-b-xl gap-x-12">
-                            <Link to="/" className="hover:text-white/90 min-[320px]:text-[16px] lg:text-[14px]">
+                        <div className="min-[320px]:bg-slate-900/95 min-[320px]:flex min-[320px]:flex-col lg:flex-row lg:bg-transparent min-[320px]:py-4 min-[320px]:px-6   min-[320px]:rounded-xl gap-x-12">
+                            <Link
+                                to="/"
+                                className="hover:text-white/90 min-[320px]:text-[16px] min-[320px]:border-b-[1px] min-[320px]:border-white/5 min-[320px]:py-4 lg:text-[14px]"
+                            >
                                 Home
                             </Link>
                             <Link
                                 to="/genre"
                                 onMouseEnter={() => alert('test')}
-                                className="hover:text-white/90 min-[320px]:text-[16px] lg:text-[14px]"
+                                className="hover:text-white/90 min-[320px]:text-[16px] min-[320px]:border-b-[1px] min-[320px]:border-white/5 min-[320px]:py-4 lg:text-[14px]"
                             >
                                 Genre
                             </Link>
-                            <Link to="/movies" className="hover:text-white/90 min-[320px]:text-[16px] lg:text-[14px]">
+                            <Link
+                                to="/movies"
+                                className="hover:text-white/90 min-[320px]:text-[16px] min-[320px]:border-b-[1px] min-[320px]:border-white/5 min-[320px]:py-4 lg:text-[14px]"
+                            >
                                 Movies
                             </Link>
-                            <Link to="/tvseries" className="hover:text-white/90 min-[320px]:text-[16px] lg:text-[14px]">
+                            <Link to="/tvseries" className="hover:text-white/90 min-[320px]:text-[16px]  min-[320px]:py-4 lg:text-[14px]">
                                 TV-Series
                             </Link>
                         </div>
