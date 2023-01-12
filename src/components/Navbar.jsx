2b62900ee290f9fc8d7615ef886f493b1d99855b
mesifer <brosfandy@gmail.com>
2023-01-12 15:52:42 +0700

    search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom';
 import { Menu2, Search, BrandGithub } from 'tabler-icons-react';
 export default function Navbar() {
     const [color, setColor] = useState(false);
-    const [isActive, setIsActive] = useState(false);
+    const [isSearch, setIsSearch] = useState(false);
+    const [isMenu, setIsMenu] = useState(false);
     const changeColor = () => {
         window.scrollY >= 90 ? setColor(true) : setColor(false);
     };
 
     const handleToggle = () => {
-        setIsActive(!isActive);
+        setIsMenu(!isMenu);
+    };
+    const searchToggle = () => {
+        setIsSearch(!isSearch);
     };
 
     window.addEventListener('scroll', changeColor);
@@ -30,14 +34,32 @@ export default function Navbar() {
                     <div className="Menubar lg:hidden">
                         <Menu2 onClick={handleToggle} size={32} color="white"></Menu2>
                     </div>
-                    <div
-                        className={
-                            isActive
-                                ? 'Navigasi text-[#ffffff8C] font-normal lg:static min-[320px]:flex-col min-[320px]:w-[100%] lg:w-[40%] min-[320px]:left-0 min-[320px]:px-4 lg:flex  min-[320px]:absolute min-[320px]:top-[120%] gap-x-8 md:pl-[60%] lg:pl-0'
-                                : 'Navigasi text-[#ffffff8C] font-normal lg:static min-[320px]:flex-col min-[320px]:w-[100%] lg:w-[40%] min-[320px]:left-0 min-[320px]:px-4 lg:flex hidden min-[320px]:absolute min-[320px]:top-[120%] gap-x-8 md:pl-[60%] lg:pl-0'
-                        }
-                    >
-                        <div className="min-[320px]:bg-slate-900/95 min-[320px]:flex min-[320px]:flex-col lg:flex-row lg:bg-transparent min-[320px]:py-4 min-[320px]:px-6   min-[320px]:rounded-xl gap-x-12">
+                    <div className="Navigasi text-[#ffffff8C] font-normal lg:static min-[320px]:flex-col min-[320px]:w-[100%] lg:w-[40%] min-[320px]:left-0 min-[320px]:px-4 lg:flex  min-[320px]:absolute min-[320px]:top-[120%] gap-x-8 md:pl-[60%] lg:pl-0">
+                        <div
+                            className={
+                                isSearch
+                                    ? 'mb-4 flex justify-between flex-row md:hidden  bg-white/50 w-full rounded-md'
+                                    : 'mb-4 hidden justify-between flex-row  bg-white/50 w-full rounded-md'
+                            }
+                        >
+                            <input
+                                className="w-5/6 px-8 py-2 bg-transparent placeholder:text-slate-800 text-slate-800 md:flex  outline-0 md:w-[40vh]"
+                                placeholder="search movies"
+                                type="text"
+                                name=""
+                                id=""
+                            />
+                            <div className="w-1/6 bg-green-700 items-center flex justify-center rounded-r-md">
+                                <Search size={24} color="white"></Search>
+                            </div>
+                        </div>
+                        <div
+                            className={
+                                isMenu
+                                    ? 'min-[320px]:bg-slate-900/95 min-[320px]:flex min-[320px]:flex-col lg:flex-row lg:bg-transparent min-[320px]:py-4 min-[320px]:px-6 min-[320px]:rounded-xl gap-x-12'
+                                    : 'hidden lg:flex lg:flex-row gap-x-12'
+                            }
+                        >
                             <Link
                                 to="/"
                                 className="hover:text-white/90 min-[320px]:text-[16px] min-[320px]:border-b-[1px] min-[320px]:border-white/5 min-[320px]:py-4 lg:text-[14px] lg:border-none"
@@ -81,7 +103,7 @@ export default function Navbar() {
                             name=""
                             id=""
                         />
-                        <Search size={24} color="white"></Search>
+                        <Search size={24} onClick={searchToggle} color="white"></Search>
                     </div>
                     <a
                         className="bg-green-700 lg:flex hidden flex-row items-center text-sm text-[#ffffffee] rounded-lg px-[4vh] py-[1vh]"
